0610314fa1d717ad0eb308520d80db89832ed73e
杉岡成哉 <a1209s518n928@gmail.com>
2026-01-01 15:48:10 +0900

    Xcode Project作成

---
+++ MarkerWalker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkerWalker
+//
+//  Created by 杉岡成哉 on 2026/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
